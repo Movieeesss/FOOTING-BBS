c23de3ac77f140004c11006cad93b52e41e8471b
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-03-08 12:22:44 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FootingBBS from './FootingBBS'
+import FootingScanner from './FootingScanner'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <FootingBBS />
+    <FootingScanner />
   </React.StrictMode>,
 )
